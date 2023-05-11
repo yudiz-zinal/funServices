345f924a61f1cddf178055167b5516a020b1ce8b
yudiz_rajnee <rajnee.m@yudiz.in>
2023-05-11 19:19:52 +0530

    Item management running

--- src/screens/admin/OwnersAdmin/ItemsManagement/CreateItem.tsx
+++ src/screens/admin/OwnersAdmin/ItemsManagement/CreateItem.tsx
@@ -1,4 +1,97 @@
+import { Col, Form, Row } from "react-bootstrap";
+import BreadcrumbNav from "src/components/BreadcrumbNav";
+import TextField from "src/components/TextField";
+
 const CreateItem = () => {
-  return "Create Item";
+  return (
+    <>
+      <BreadcrumbNav />
+      <section className="profile-section mb-3 mb-md-5">
+        <div className="title d-flex justify-content-between align-items-center pb-3">
+          <h1 class="mb-0">Add New Item</h1>
+        </div>
+        <div class="from-group">
+          <label for="name" class="form-label">
+            Item Name
+          </label>
+          <input type="text" class="form-control input-field" id="name" placeholder="Enter Name" />
+        </div>
+        <div class="from-group">
+          <label for="password" class="form-label input-label-fonts" >
+            Item Code
+          </label>
+          <input type="text" class="form-control input-field" id="name" placeholder="Enter Item Code" />
+        </div>
+        <div class="from-group">
+          <label for="password" class="form-label input-label-fonts" >
+            Barcode
+          </label>
+          <input type="text" class="form-control input-field" id="name" placeholder="Enter Barcode Number" />
+        </div>
+        <div class="from-group">
+          <label for="password" class="form-label input-label-fonts" >
+            School’s Cost
+          </label>
+          <input type="text" class="form-control input-field" id="name" placeholder="Enter School’s Cost" />
+        </div>
+        <Row>
+          <Col md={6}>
+            <div class="from-group">
+              <label for="password" class="form-label input-label-fonts" >
+                Enter Price Level 1
+              </label>
+              <input type="text" class="form-control input-field" id="name" placeholder="Enter Price 1" />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6} lg={4}>
+            <div class="from-group">
+              <label for="city" class="form-label">
+                City
+              </label>
+              <input type="text" class="form-control input-field" id="city" placeholder="Enter City" />
+            </div>
+          </Col>
+          <Col md={6} lg={4}>
+            <div class="from-group">
+              <label for="state" class="form-label">
+                State
+              </label>
+              <input type="text" class="form-control input-field" id="state" placeholder="Enter State" />
+            </div>
+          </Col>
+          <Col md={6} lg={4}>
+            <div class="from-group">
+              <label for="zipCode" class="form-label">
+                Zip Code
+              </label>
+              <input type="text" class="form-control input-field" id="zipCode" placeholder="Enter Zip Code" />
+            </div>
+          </Col>
+        </Row>
+        <div class="from-group">
+          <label for="email" class="form-label">
+            Volunteer ID
+          </label>
+          <input
+            type="text"
+            class="form-control input-field"
+            id="email"
+            placeholder="Enter Email ID"
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button className="theme-btn small-btn me-2 me-md-4">
+            Save
+          </button>
+          <button className="theme-btn small-btn light-btn">
+            Cancel
+          </button>
+        </div>
+
+      </section>
+    </>
+  );
 };
 export default CreateItem;
--- src/screens/admin/OwnersAdmin/Layout/index.tsx
+++ src/screens/admin/OwnersAdmin/Layout/index.tsx
@@ -1,22 +1,29 @@
+import { useState } from "preact/hooks";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, Button } from "react-bootstrap";
 
 import { logout } from "src/services/AuthService";
 
 import logo from "../../../../assets/images/logo.png";
+import menuIcon from "../../../../assets/images/icon/menu-icon.svg";
 import userPic from "../../../../assets/images/dummy/user-img.jpg";
 
 const OwnerAdminLayout = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   const logoutUser = async () => {
     await logout();
     navigate("/login");
   };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="d-flex">
-      <aside className="sidebar">
+      <aside className={`sidebar ${isMenuOpen && 'active'}`}>
         <NavLink href="/" className="owner-logo d-flex justify-content-center">
-          <img src={logo} />
+          <img src={logo} alt="logo" />
         </NavLink>
         <nav className="menu">
           <NavLink to="/">Dashboard</NavLink>
@@ -27,8 +34,11 @@ const OwnerAdminLayout = () => {
         </nav>
       </aside>
       <div className="content-owner">
-        <header className="owner-header d-flex justify-content-end">
-          <Dropdown>
+        <header className="owner-header d-flex justify-content-between align-items-center">
+          <NavLink href="/" className="owner-logo d-block d-xl-none">
+            <img src={logo} alt="logo" />
+          </NavLink>
+          <Dropdown className="ms-auto">
             <Dropdown.Toggle
               variant="link"
               id="userDropdown"
@@ -43,6 +53,9 @@ const OwnerAdminLayout = () => {
               <Dropdown.Item onClick={async () => logoutUser()}>Logout</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
+          <Button variant="link" className="menu-icon ms-1 ms-sm-2 d-block d-xl-none" onClick={() => toggleMenu()}>
+            <img src={menuIcon} alt="menu" className="w-100" />
+          </Button>
         </header>
         <main class="main-content">
           <Outlet />
